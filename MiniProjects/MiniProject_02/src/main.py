f350db62ce0b0559e5d6f8b3d8e769bef16e1a83
Dushan Terzikj <dusanterzic87@gmail.com>
2019-04-16 00:26:19 +0200

    :memo: Divide data into training and testing :memo: :bug: There might be a bug with K-Means, check issues :bug:

--- MiniProjects/MiniProject_02/src/main.py
+++ MiniProjects/MiniProject_02/src/main.py
@@ -1,21 +1,10 @@
 import matplotlib.pyplot as plt
-import random
 import numpy as np
 from k_means import KMeans
 
-if __name__ == "__main__":
-    # load mfeat-pix dataset
-    pixel_data = np.loadtxt('../DigitsBasicRoutines/mfeat-pix.txt', dtype=np.uint)
-
-    K = 10
-    km = KMeans(K)
-    print("Doing K-means clustering with K={}".format(K))
-    cb_vectors = km.fix(pixel_data)
-    print("Done!")
-
-    # draw codebook vector for specified cluster
+def tryout(cb_vectors):
+    """ Just for checking the codebook vectors, ignore this function. """
     while True:
-
         cluster_idx = input("Input a number: ")
         if not cluster_idx:
             break
@@ -24,3 +13,28 @@ if __name__ == "__main__":
             break
         plt.imshow(cb_vectors[cluster_idx,:].reshape(16,15))
         plt.show()
+
+
+
+# load mfeat-pix dataset
+print("Loading pixel data from file...")
+pixel_data = np.loadtxt('../DigitsBasicRoutines/mfeat-pix.txt', dtype=np.uint)
+print("Done!")
+
+training_data = []
+test_data = []
+
+# Divide the test and training data into separate sets
+for i in range(0, len(pixel_data), 200):
+    for j in range(i, i+100):
+        training_data.append(pixel_data[j])
+    for j in range(i+100, i+200):
+        test_data.append(pixel_data[j])
+
+K = 10
+km = KMeans(K)
+print("Doing K-means clustering with K={}".format(K))
+cb_vectors = km.fix(pixel_data)
+print("Done!")
+tryout(cb_vectors)
+
