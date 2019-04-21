c696e221b274b5e33ee9da6bd3d48d0ac009354b
fmanana <fezile.manana@protonmail.com>
2019-04-21 16:22:23 +0200

    adding pca

---
+++ MiniProjects/MiniProject_02/src/pca.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+# load mfeat-pix dataset
+pixel_data = np.loadtxt('../DigitsBasicRoutines/mfeat-pix.txt', dtype=np.uint)
+
+class pca:
+    """
+    :param m: the number of principle components we wish to obtain
+    :param data: the original data set
+    """
+    def __init__(self, m, data):
+        self.m = m
+        self.data = data
+    
+    """ computes the mean of the cloud data """
+    def __mean(self):
+        counter = 0
+        mean = np.zeros([len(self.data[0])]).reshape(len(self.data[0]),)
+        for point in self.data:
+            mean += point
+            counter += 1
+        mean = mean / counter
+        return mean
+
+    """ returns a list object with the centered data cloud """
+    def __centerData(self):
+        mean = self.__mean()
+        centeredData = np.zeros([len(self.data) * len(self.data[0])]).reshape(len(self.data), len(self.data[0]))
+
+        for i in range(len(self.data)):
+            centeredData[i] = (self.data[i] - mean)
+
+        return centeredData
+
+    def cov(self):
+        data = self.__centerData()
+        n = len(self.data)
+
+        covariance = np.dot(data, np.transpose(data)) / n
+
+        return covariance
+
+    def svd(self):
+        cov = self.cov()
+        u, s, vh = np.linalg.svd(cov)
+        
+        return u, s, vh
+        
+
+pca = pca(10, pixel_data)
+
+if __name__ == "__main__":
+    print(pca.svd())
